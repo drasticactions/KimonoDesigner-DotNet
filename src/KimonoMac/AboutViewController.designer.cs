cd2f5ed3907684936b76fae745a8e5b8d172fa77
Timothy Miller <innerlogic4321@gmail.com>
2024-07-29 16:36:01 +0900

    Add Code

--- src/KimonoMac/AboutViewController.designer.cs
+++ src/KimonoMac/AboutViewController.designer.cs
@@ -13,11 +13,19 @@ namespace KimonoMac
 	[Register ("AboutViewController")]
 	partial class AboutViewController
 	{
+		[Outlet]
+		AppKit.NSTextField VersionField { get; set; }
+
 		[Action ("ShowIcons8License:")]
 		partial void ShowIcons8License (Foundation.NSObject sender);
 
 		void ReleaseDesignerOutlets ()
 		{
+			if (VersionField != null) {
+				VersionField.Dispose ();
+				VersionField = null;
+			}
+
 		}
 	}
 }
